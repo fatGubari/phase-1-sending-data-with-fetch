e06b5f8a045462ff4e7973d2b7030f7da2c89a4f
fatima abdullah <fatimajubary@gmail.com>
2023-01-23 16:44:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+// const form = document.querySelector('#data').addEventListener('submit', (e) => {
+//       e.preventDefault()
+        let Name = document.querySelector('#name')
+        let email = document.querySelector('#email')
+        console.log(submitData(Name,email))
+// })
+function submitData(name,email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+          })
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (users) {
+        console.log(users.name,users.email, users.id);
+      })
+      .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+        });
+  }
+
+  
+
